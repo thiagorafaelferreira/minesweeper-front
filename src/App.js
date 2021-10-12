eb9c174551a0191c3bc24d2b619f495b78b1f7d1
thiagorafaelferreira <thiagorafaelferreira@gmail.com>
2021-10-11 21:30:12 -0300

    initial project commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,123 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const urlNewGame = "http://ec2-54-94-15-216.sa-east-1.compute.amazonaws.com:8080/new_game"
+const urlFlagPosition = "http://ec2-54-94-15-216.sa-east-1.compute.amazonaws.com:8080/flag_position"
+const urlFieldPosition = "http://ec2-54-94-15-216.sa-east-1.compute.amazonaws.com:8080/click_position"
+const urlReset = "http://ec2-54-94-15-216.sa-east-1.compute.amazonaws.com:8080/reset_game"
 
 function App() {
+
+  const [gameMatch, setGameMatch] = useState({});
+  const [numberRows, setNumberRows] = useState(10);
+  const [numberColumns, setNumberColumns] = useState(10);
+  const [numberMinesOnField, setNumberMinesOnField] = useState(10);
+
+  const clickResetNewGame = () => {
+    reset();
+  }
+
+  const reset = () => {
+    let headers = {
+      headers: {
+          'user_id': '123456',
+          'Content-Type': 'application/json'
+      }
+    }
+
+    axios.delete(urlReset, headers).then(response => {
+      console.log(response);
+      if(response.data === "DELETED") {
+        newGame();
+      }
+    }).catch(() => {
+      console.log("erro ao requisitar api")
+    })
+  }
+
+  const validateGameOver = (gameMatch) => {
+    if(gameMatch && gameMatch.status === "GAME_OVER") {
+      alert("Você perdeu, inicie uma nova partida");
+      return true;
+    }
+  }
+
+  const clickFieldPosition = (evt, row, column) => {
+    evt.preventDefault();
+    if(validateGameOver(gameMatch)) return;
+
+    let headers = {
+      headers: {
+          'user_id': '123456',
+          'Content-Type': 'application/json'
+      }
+    }
+
+    axios.put((evt.button === 0 ? urlFieldPosition : urlFlagPosition) + `/${row}/${column}`, {}, headers).then(response => {
+      setGameMatch(response.data)
+      validateGameOver(response.data);
+    }).catch(() => {
+      console.log("erro ao requisitar api")
+    })  
+  }
+
+  const newGame = () => {
+    const requestBody = {
+      numberRows: numberRows,
+      numberColumns: numberColumns,
+      numberMinesOnField: numberMinesOnField
+    }
+
+    let headers = {
+      headers: {
+          'user_id': '123456',
+          'Content-Type': 'application/json'
+      }
+    }
+
+    axios.post(urlNewGame, requestBody, headers).then(response => {
+      console.log(response);
+      setGameMatch(response.data)
+    }).catch(() => {
+      console.log("erro ao requisitar api")
+    })
+  }
+
+  useEffect(() => {
+    newGame();
+  }, [setGameMatch]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="button" value="New game" onClick={clickResetNewGame} />
+        <div>
+          <div className="config-title">Rows</div>
+          <div className="config-title">Columns</div>
+          <div className="config-title">Mines</div>
+        </div>
+        <div className="area_config">
+          <input type="number" className="config" value={numberRows} onChange={ev => setNumberRows(ev.target.value)} onKeyDown="return false" />
+          <input type="number" className="config" value={numberColumns} onChange={ev => setNumberColumns(ev.target.value)} onKeyDown="return false" />
+          <input type="number" className="config" value={numberMinesOnField} onChange={ev => setNumberMinesOnField(ev.target.value)} onKeyDown="return false" />
+        </div>
+        <div onContextMenu={(e)=>  {e.preventDefault(); return false;}}>
+          {gameMatch && gameMatch.mineField &&
+              gameMatch.mineField.map(field => <div className="row">
+                {field.map(f => <div id={f.id} 
+                  onMouseDown={evt => clickFieldPosition(evt, f.rowPosition, f.columnPosition)}
+                  className={`${
+                    gameMatch.status === "GAME_OVER" && f.mine
+                      ? "field field-red" 
+                      : (f.flaged 
+                          ? "field field-yellow"
+                          : (f.clicked 
+                              ? "field field-blue"
+                              : "field"))
+                  }`}>{f.minesArround > 0 ? f.minesArround : ""}</div>)}
+            </div>)}
+        </div>
       </header>
     </div>
   );
